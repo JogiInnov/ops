97cc1b853b6f2c4224b96502b86af8baea497957
JogiInnov <joginder.kumar@innovaccer.com>
2019-03-27 19:51:59 +0530

    update ebs_available.sh

--- aws-cli/ebs_available.sh
+++ aws-cli/ebs_available.sh
@@ -4,21 +4,68 @@
 
 #!/bin/bash
 
-#echo "`aws ec2 describe-volumes  --filters Name=status,Values=available`" > json.txt
+
+export TERM=xterm
+source ~/.profile
+UseR=`id -nu`
+#-- Log Path
+Log_PATH=Logrotate.log
+
+Logger()
+{
+        Colorscheme=$2
+        if [ ! $Colorscheme ]
+        then
+                Colorscheme=9
+        fi
+        tput setf $Colorscheme
+        Logdate=`date +%Y%m%d:%H:%M:%S`
+                echo $Logdate :: $1 | tee -a $Log_PATH
+        tput sgr0
+}
+
+
+##################
+##Initialization##
+##################
+
+echo "`aws ec2 describe-volumes  --filters Name=status,Values=available`" > json.txt
 count=`cat json.txt  | jq '.[] | length'`
-#echo "count is => $count"
 i="0"
+
 while [ $i -lt $count ]
 do 
 	#echo $i
-	#echo `cat json.txt | jq '.[]['$i'].VolumeId'` 
 	idsArray+=( "$( echo "`cat json.txt | jq '.[]['$i'].VolumeId'`" )" )
 	i=`expr $i + 1`
 done
 
-echo "=========printing state=available volume id's as below===="
+echo "========= printing available volume id's as below ===="
+
 for i in ${idsArray[@]}
-do echo $i
-done
+	do 
+		#echo $i
+		i=$(echo $i | sed s/\"//g)
+        	Logger "[ Info ] :: Volume id found ==> $i ..!!" 4
+		echo "************ Creating snapshot for volume $i below: ****************"
+		outputvar=$(echo `aws ec2 create-snapshot --volume-id $i --description 'ebs snapshot form aws-cli' --tag-specifications 'ResourceType=snapshot,Tags=[{Key=purpose,Value=prod},{Key=costcenter,Value=123}]' 2>&1`)
+		if [ $? -eq "0" ]
+			then
+				sleep 1 
+				Logger "[ Info ] :: Snapshot created ..!! with below details: ${outputvar}" 3
+				echo "------------------------------------------"
+				Logger "[ Info ] :: Deleting Volume with id == $i ..!!" 6
+				outputvar=$(echo `aws ec2 delete-volume --volume-id $i --dry-run 2>&1`)
+                                sleep 1
+				Logger "[ Alert ] :: Volume deleted ..!! with below details: ${outputvar}" 6
+				
+			else
+				sleep 1
+				Logger "[ Info ] :: Error creating snapshot ..!! ${outputvar}" 3
+		fi
+				echo " ------------------------------" >> $Log_PATH
+
+	done
+
 
 
